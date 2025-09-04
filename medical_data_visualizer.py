0fac6b375be9395349f9e4170e8be6796dc7e422
Victor Grycajuk <victorgryca@gmail.com>
2025-09-04 08:33:01 -0300

    finishes

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,33 +3,47 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
+#1
+
 df = pd.read_csv('medical_examination.csv')
+df.head()
 
 # 2
 
-BMI = df['Weight']/(df['Height']/100)^2
-df['overweight'] = np.where((df['Weight']/(df['Height']/100)^2) > 25, '1', '0')
+df['overweight'] = np.where((df['weight']/(df['height']/100)**2) > 25, '1', '0')
 
-# 3
+
+#3 
+
+df['cholesterol'] = np.where(df['cholesterol'] > 1, 1, 0)
+df['gluc'] = np.where(df['gluc'] > 1, 1, 0)
 
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+        # 5
+    df_cat = pd.melt(
+                df,
+                id_vars=['cardio'],
+                value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],
+                var_name='variable',
+                value_name='value'
+    )      
 
-
-    # 6
-    df_cat = None
-    
+        # 6
+    df_cat = (
+                df_cat
+                .groupby(['cardio', 'variable', 'value'], observed=True)
+                .size()
+                .reset_index(name='total')
+    )    
 
     # 7
-
+    fig = sns.catplot(x = "variable", y = "total", data = df_cat, hue = "value", kind = "bar", col = "cardio").fig
 
 
     # 8
-    fig = None
+    
 
 
     # 9
@@ -40,23 +54,37 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[(df['ap_lo']<=df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))]
+    
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))    
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 10), dpi=120) 
 
-    # 15
+     # 15
+    sns.heatmap(
+        corr,
+        mask=mask,                 
+        annot=True,               
+        fmt=".1f",
+        vmin=-1, vmax=1, center=0, 
+        cmap="coolwarm",
+        square=True,
+        linewidths=.5,
+        cbar_kws={"shrink": .75, "label": "Pearson r"},
+        ax=ax
+    )
 
-
-
-    # 16
+     # 16
     fig.savefig('heatmap.png')
     return fig
+    
+    
+
+    
+    
